5d802ca5008d41432d0612bc854e7a34e6dda204
Ibra <Ibrahim-1991@mail.ru>
2023-01-25 18:14:53 +0300

    create products pages

---
+++ views/AllProducts.jsx
@@ -0,0 +1,12 @@
+const React = require('react');
+const ProductPage = require('./ProductPage');
+
+module.exports = function Allproducts({ allProducts, courierProduct, user }) {
+  return (
+    <div className="products-container">
+      {allProducts.map((product) => (
+        <ProductPage product={product} courierProduct={courierProduct} user={user} key={product.id} />
+      ))}
+    </div>
+  );
+};
