393598b062ac54a52f70222e894b3fb9ff24ff1d
Ibra <Ibrahim-1991@mail.ru>
2023-01-26 11:57:11 +0300

    fix db probs

--- db/models/product.js
+++ db/models/product.js
@@ -1,25 +1,26 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Product extends Model {
-    static associate({User}) {
+    static associate({ User }) {
       this.hasMany(User, {
-        foreignKey: "productId",
+        foreignKey: 'productId',
       });
     }
   }
-  Products.init({
-    image: {type:DataTypes.TEXT,allowNull:false},
-    title: {type:DataTypes.TEXT,allowNull:false},
-    price: {type:DataTypes.INTEGER,allowNull:false},
-    discontPrice: {type:DataTypes.INTEGER,allowNull:false},
-    status: {DataTypes.TEXT},
-    adress: {type:DataTypes.TEXT,allowNull:false},
-  }, {
-    sequelize,
-    modelName: 'Product',
-  });
+  Product.init(
+    {
+      image: { type: DataTypes.TEXT, allowNull: false },
+      title: { type: DataTypes.TEXT, allowNull: false },
+      price: { type: DataTypes.INTEGER, allowNull: false },
+      discontPrice: { type: DataTypes.INTEGER, allowNull: false },
+      status: { type: DataTypes.TEXT },
+      adress: { type: DataTypes.TEXT, allowNull: false },
+    },
+    {
+      sequelize,
+      modelName: 'Product',
+    },
+  );
   return Product;
 };
