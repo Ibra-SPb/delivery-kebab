b4967b1da7388e23cfac08a9eebc948605861177
eddy49reg <akhnazarov1990@gmail.com>
2023-01-27 14:07:02 +0300

    add styles for customer

--- views/AllProducts.jsx
+++ views/AllProducts.jsx
@@ -1,31 +1,26 @@
 /* eslint-disable react/prop-types */
-const React = require('react');
-const AllOrdersPage = require('./AllOrdersPage');
-const ProductPage = require('./ProductPage');
+const React = require("react");
+const AllOrdersPage = require("./AllOrdersPage");
+const ProductPage = require("./ProductPage");
 
 module.exports = function Allproducts({ orders, user, allOrders }) {
-  console.log(allOrders);
   return (
     <div>
       {!user ? (
         <div>
-          <h3>Доступные заказы</h3>
-          {orders.map((order) => (
-            <div>
+          <h3 className="title">Доступные заказы:</h3>
+          <div className="orderTable">
+            {orders.map((order) => (
               <AllOrdersPage order={order} key={order.id} />
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       ) : (
         <div>
-          <div className="products-container">
-            {orders.map((order) => (
-              <div>
-                <h3>Ваши заказы: </h3>
-                <div>
-                  <ProductPage order={order} user={user} key={order.id} />
-                </div>
-              </div>
+          <h3 className="title">Доступные заказы: </h3>
+          <div className="products-container orderTable">
+            {allOrders.map((order) => (
+              <ProductPage order={order} user={user} key={order.id} />
             ))}
           </div>
           <br />
