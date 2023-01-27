da58afb1eb6b02c3736da9fa26d95261f881eae3
Slimshady3133 <i.puzakov2010@mail.ru>
2023-01-27 10:02:07 +0300

    fix problem

--- views/Reg.jsx
+++ views/Reg.jsx
@@ -1,20 +1,15 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 const React = require('react');
 const Layout = require('./Layout');
 
 function Reg({ title }) {
   return (
     <Layout title={title}>
-
-      <div>
-        <div>
-          <h1>Регистрация</h1>
-
       <div className="forma">
         <div>
           <h1>Регистрация</h1>
         </div>
         <div>
-
           <form className="regForm" action="/reg" method="post">
             <label>
               Имя:
@@ -29,9 +24,8 @@ function Reg({ title }) {
               <input name="phone" type="text" placeholder="Введите номер" />
             </label>
             <select name="role">
-
               <option>courier</option>
-
+              <option>customer</option>
             </select>
             <label>
               Адрес:
@@ -39,11 +33,7 @@ function Reg({ title }) {
                 name="address"
                 type="text"
                 placeholder="Введите адрес"
-
                 id="tryApi"
-
-                id="Goo"
-
               />
             </label>
             <label>
@@ -59,13 +49,9 @@ function Reg({ title }) {
           </form>
         </div>
 
-        <div className="errorBlock"></div>
-      </div>
-      <div id="map" style={{ width: '600px', height: '400px' }}></div>
-
         <div className="errorBlock" />
       </div>
-
+      <div id="map" style={{ width: '600px', height: '400px' }} />
     </Layout>
   );
 }
