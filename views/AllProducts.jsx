96eebea932b858e6dff851d2cfe9422632b114cf
Ibra <Ibrahim-1991@mail.ru>
2023-01-27 13:00:00 +0300

    fix allproducts page

--- views/AllProducts.jsx
+++ views/AllProducts.jsx
@@ -4,28 +4,34 @@ const AllOrdersPage = require('./AllOrdersPage');
 const ProductPage = require('./ProductPage');
 
 module.exports = function Allproducts({ orders, user, allOrders }) {
+  console.log(allOrders);
   return (
     <div>
-      <div className="products-container">
-        {orders.map((order) => (
-          <div>
-            <h3>Ваши заказы: </h3>
+      {!user ? (
+        <div>
+          <h3>Доступные заказы</h3>
+          {orders.map((order) => (
             <div>
-              <ProductPage order={order} user={user} key={order.id} />
+              <AllOrdersPage order={order} key={order.id} />
             </div>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <div className="products-container">
+            {orders.map((order) => (
+              <div>
+                <h3>Ваши заказы: </h3>
+                <div>
+                  <ProductPage order={order} user={user} key={order.id} />
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <br />
-      <br />
-      <div>
-        <h3>Доступные заказы</h3>
-        {allOrders.map((order) => (
-          <div>
-            <AllOrdersPage order={order} user={user} key={order.id} />
-          </div>
-        ))}
-      </div>
+          <br />
+          <br />
+        </div>
+      )}
     </div>
   );
 };
--- views/Main.jsx
+++ views/Main.jsx
@@ -4,36 +4,37 @@ const Layout = require('./Layout');
 const AllProducts = require('./AllProducts');
 const Courier = require('./Courier');
 
-module.exports = function Main({
-  title, user, products, orders, allOrders,
-}) {
+module.exports = function Main({ title, user, products, orders, allOrders }) {
   // console.log(user.role);
   return (
     <Layout title={title} user={user}>
       {user && user.role === 'customer' && (
         <div>
           {/* {orders.map((order) => <span key={order.id}>{order.product.title}</span>)} */}
-          <AllProducts user={user} products={products} orders={orders} allOrders={allOrders} />
+          <AllProducts
+            user={user}
+            products={products}
+            orders={orders}
+            allOrders={allOrders}
+          />
         </div>
-      )} 
+      )}
       {!user && (
         <div>
           <AllProducts products={products} orders={orders} />
         </div>
-      ) }
-      {user
-        && (user.role === 'courier')
-        && (
-          // прорисовка для курьера
-          <div>
-            <Courier
-              title={title}
-              user={user}
-              products={products}
-              orders={orders}
-            />
-          </div>
-        )}
+      )}
+      {user && user.role === 'courier' && (
+        // прорисовка для курьера
+        <div>
+          <Courier
+            title={title}
+            user={user}
+            products={products}
+            orders={orders}
+          />
+        </div>
+      )}
     </Layout>
   );
 };
