45a829e140851a14e4515e19480b024fed387ee3
eddy49reg <akhnazarov1990@gmail.com>
2023-01-27 14:23:32 +0300

    fix some bugs

--- views/Main.jsx
+++ views/Main.jsx
@@ -1,17 +1,18 @@
 /* eslint-disable react/prop-types */
-const React = require('react');
-const Layout = require('./Layout');
-const AllProducts = require('./AllProducts');
-const Courier = require('./Courier');
+const React = require("react");
+const Layout = require("./Layout");
+const AllProducts = require("./AllProducts");
+const Courier = require("./Courier");
 
 module.exports = function Main({ title, user, products, orders, allOrders }) {
   // console.log(user.role);
   return (
     <Layout title={title} user={user}>
-      {user && user.role === 'customer' && (
+      {user && user.role === "customer" && (
         <div>
-          <div>Ты зарегался</div>
-          {/* {orders.map((order) => <span key={order.id}>{order.product.title}</span>)} */}
+          {orders.map((order) => (
+            <span key={order.id}>{order.product.title}</span>
+          ))}
 
           <AllProducts
             user={user}
@@ -19,16 +20,14 @@ module.exports = function Main({ title, user, products, orders, allOrders }) {
             orders={orders}
             allOrders={allOrders}
           />
-
         </div>
       )}
       {!user && (
         <div>
-          <div>Зарегайся</div>
-          {/* <AllProducts products={products} orders={orders} /> */}
+          <AllProducts products={products} orders={orders} />
         </div>
       )}
-      {user && user.role === 'courier' && (
+      {user && user.role === "courier" && (
         // прорисовка для курьера
         <div>
           <Courier
